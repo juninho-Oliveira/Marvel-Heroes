81a17af7eceab7e40553206cb0f24fda6e347971
AndersonS7 <andersondez290@gmail.com>
2024-02-21 21:27:16 -0300

    sistema de busca por nome foi concluído

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,7 +3,7 @@ import './styles.css';
 import Title from "../../components/Title";
 import Button from "../../components/Button";
 import personagensData from "../../utils/herois";
-import { useState } from "react";
+import { useState, useEffect, useContext } from "react";
 import Card from "../../components/Card";
 import Header from "../../components/Header";
 import Filter from "../../components/Filter";
@@ -11,21 +11,33 @@ import Filter from "../../components/Filter";
 const HomePage = () => {
   const [personagens, setPersonagem] = useState(personagensData);
 
+  const handleSearch = (data) => {
+    search(data);
+  }
+
+  function search(param) {
+    let data;
+    if (param !== '') {
+      data = personagensData.filter((item) => item.nome.toUpperCase() === param.toUpperCase());
+      setPersonagem(data);
+    } else {
+      setPersonagem(personagensData);
+    }
+  }
+
   function filterPersonagem(tipo) {
     let data;
     if (tipo !== '') {
       data = personagensData.filter((item) => item.tipo === tipo);
       setPersonagem(data);
-    }else{
+    } else {
       setPersonagem(personagensData);
-
     }
   }
 
-
   return (
     <section>
-      <Header />
+      <Header onHanderSearch={(data) => handleSearch(data)} />
       <Filter onIncrement={(tipo) => filterPersonagem(tipo)} personagens={personagens} />
       <div className="App-card">
         {personagens.map((personagem) =>
